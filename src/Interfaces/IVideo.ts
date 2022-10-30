3f23809f1a2fa76f4b7c04a2030825d75e82ec72
Ashary Vermaysha <asharyver13@gmail.com>
2022-10-30 17:41:10 +0700

    video interface

---
+++ src/Interfaces/IVideo.ts
@@ -0,0 +1,16 @@
+export interface IVideo {
+  /**
+   * Source URL of the video.
+   */
+  url: string
+
+  /**
+   * Height of the video.
+   */
+  height?: number
+
+  /**
+   * Width of the video.
+   */
+  width?: number
+}
